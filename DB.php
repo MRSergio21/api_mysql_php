38eeb8fffeee0391ce50c86a53fe2d13b010d738
lj <ljgestion.docente@gmail.com>
2023-03-23 13:48:48 +0100

    subir base de datos mysql para trabajo con mongo db

--- DB.php
+++ DB.php
@@ -8,9 +8,9 @@ class DB{
 
     public function __construct(){
        $this->host= 'localhost';
-        $this->db= 'uni_2022_student';
+        $this->db= 'university_2023_caso_mongodb';
         $this->user= 'root';
-        $this->password ='root';
+        $this->password ='';
         //$this->charset = 'utf8mb4';*/
     }
     function connect(){
